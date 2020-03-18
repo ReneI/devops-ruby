fca53970b1e2ceffef1c1337de0352101295a95d
rene ibarra <reidisy@gmail.com>
2020-03-18 11:40:37 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+b8137ef4a49dadfe0cfeb170f838f6d6742177af
+sysctl -w vm.max_map_count=262144
+sysctl -w fs.file-max=65536
 
+cd dock
 # Start memory 
 
 # docker-compose up
+docker-compose -d up
